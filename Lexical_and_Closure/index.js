31e452c250e500d57fd4c932f6ad62e2096defd5
ranjan3nov <ranjan3nov@gmail.com>
2024-01-05 23:36:50 +0530

    lexical_and_closure

---
+++ Lexical_and_Closure/index.js
@@ -0,0 +1,14 @@
+function init() {
+    let name = "Mozilla";
+    function displayName() {
+
+        // console.log(name);
+        function displayName2() {
+
+            console.log(name);
+        }
+        displayName2();
+    }
+    displayName();
+}
+init();
